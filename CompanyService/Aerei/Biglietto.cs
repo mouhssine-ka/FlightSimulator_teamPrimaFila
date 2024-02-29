d19683d0bdae3d0ea08f664a96c1f31e6adf8595
mouhssine <moussuper@gmail.com>
2024-02-29 11:04:38 +0100

    Aggiunto BiglietoBl e BigliettoApi

--- CompanyService/Aerei/Biglietto.cs
+++ CompanyService/Aerei/Biglietto.cs
@@ -2,8 +2,18 @@
 
 public class Biglietto
 {
+    public int BigliettoId {get; set;}
+    public Volo Volo {get; set;}
+    public int PostiPrenotati {get; set;}
+    public double ImportoTotale {get; set;}
+    public DateTime DataAcquisto {get; set;}
 
-    public Biglietto()
+    public Biglietto(int bigliettoId, Volo volo, int postiPrenotati, double importoTotale, DateTime dataAcquisto)
     {
+        BigliettoId = bigliettoId;
+        Volo = volo;
+        PostiPrenotati = postiPrenotati;
+        ImportoTotale = importoTotale;
+        DataAcquisto = dataAcquisto;
     }
 }
--- CompanyService/Contracts/BigliettoApi.cs
+++ CompanyService/Contracts/BigliettoApi.cs
@@ -2,7 +2,17 @@
 
 public class BigliettoApi
 {
-    public BigliettoApi()
+    public int BigliettoId {get; set;}
+    public Volo Volo {get; set;}
+    public int PostiPrenotati {get; set;}
+    public double ImportoTotale {get; set;}
+    public DateTime DataAcquisto {get; set;}
+    public BigliettoApi(int bigliettoId, Volo volo, int postiPrenotati, double importoTotale, DateTime dataAcquisto)
     {
+        BigliettoId = bigliettoId;
+        Volo = volo;
+        PostiPrenotati = postiPrenotati;
+        ImportoTotale = importoTotale;
+        DataAcquisto = dataAcquisto;
     }
 }
