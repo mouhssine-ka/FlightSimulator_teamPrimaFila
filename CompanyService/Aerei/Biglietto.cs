f13a5f416d7d3d2ee4dfad1be7639f8f6524eca9
mouhssine <moussuper@gmail.com>
2024-02-29 22:33:11 +0100

    Modificato il tipo di ImportoTotale

--- CompanyService/Aerei/Biglietto.cs
+++ CompanyService/Aerei/Biglietto.cs
@@ -5,13 +5,13 @@ public class Biglietto
     public int BigliettoId {get; set;}
     public Volo Volo {get; set;}
     public int PostiPrenotati {get; set;}
-    public double ImportoTotale {get; set;}
+    public decimal ImportoTotale {get; set;}
     public DateTime DataAcquisto {get; set;}
 
     public Biglietto(){
         
     }
-    public Biglietto(int bigliettoId, Volo volo, int postiPrenotati, double importoTotale, DateTime dataAcquisto)
+    public Biglietto(int bigliettoId, Volo volo, int postiPrenotati, decimal importoTotale, DateTime dataAcquisto)
     {
         BigliettoId = bigliettoId;
         Volo = volo;
@@ -19,7 +19,7 @@ public class Biglietto
         ImportoTotale = importoTotale;
         DataAcquisto = dataAcquisto;
     }
-    public Biglietto(Volo volo, int postiPrenotati, double importoTotale, DateTime dataAcquisto)
+    public Biglietto(Volo volo, int postiPrenotati, decimal importoTotale, DateTime dataAcquisto)
     {
         Volo = volo;
         PostiPrenotati = postiPrenotati;
--- CompanyService/Contracts/BigliettoApi.cs
+++ CompanyService/Contracts/BigliettoApi.cs
@@ -5,9 +5,9 @@ public class BigliettoApi
     public int BigliettoId {get; set;}
     public Volo Volo {get; set;}
     public int PostiPrenotati {get; set;}
-    public double ImportoTotale {get; set;}
+    public decimal ImportoTotale {get; set;}
     public DateTime DataAcquisto {get; set;}
-    public BigliettoApi(int bigliettoId, Volo volo, int postiPrenotati, double importoTotale, DateTime dataAcquisto)
+    public BigliettoApi(int bigliettoId, Volo volo, int postiPrenotati, decimal importoTotale, DateTime dataAcquisto)
     {
         BigliettoId = bigliettoId;
         Volo = volo;
