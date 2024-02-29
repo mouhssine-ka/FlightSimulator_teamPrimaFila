21e5c98a81f1a91453f2d146b8327a9899b55ecb
Federico Grassi <grfederico10@gmail.com>
2024-02-29 10:49:17 +0100

    Aggiunta classe volo e voloApi

--- CompanyService/Aerei/Volo.cs
+++ CompanyService/Aerei/Volo.cs
@@ -2,8 +2,26 @@
 
 public class Volo
 {
+    public string IdVolo { get; set; }
+    public Aereo Aereo { get; set; }
+    public long PostiRimanenti { get; set; }
+    public decimal CostoDelPosto { get; set; }
+    public string CittaPartenza { get; set; }
+    public string CittaArrivo { get; set; }
+    public DateTime OrarioPartenza { get; set; }
+    public DateTime OrarioArrivo { get; set; }
 
-    public Volo()
+    public Volo(string idVolo, Aereo aereo, long postiRimanenti, decimal costroDelPorsto,
+     string cittaPartenza, string cittaArrivo, DateTime orarioPartenza, DateTime orarioArrivo)
     {
+        IdVolo = idVolo;
+        Aereo = aereo;
+        PostiRimanenti = postiRimanenti;
+        CostoDelPosto = costroDelPorsto;
+        CittaPartenza = cittaPartenza;
+        CittaArrivo = cittaArrivo;
+        OrarioPartenza = orarioPartenza;
+        OrarioArrivo = orarioArrivo;
+        
     }
 }
--- CompanyService/Contracts/VoloApi.cs
+++ CompanyService/Contracts/VoloApi.cs
@@ -2,8 +2,25 @@
 
 public class VoloApi
 {
+    public string IdVolo { get; set; }
+    public Aereo Aereo { get; set; }
+    public long PostiRimanenti { get; set; }
+    public decimal CostoDelPosto { get; set; }
+    public string CittaPartenza { get; set; }
+    public string CittaArrivo { get; set; }
+    public DateTime OrarioPartenza { get; set; }
+    public DateTime OrarioArrivo { get; set; }
 
-    public VoloApi()
+    public VoloApi(string idVolo, Aereo aereo, long postiRimanenti, decimal costroDelPorsto,
+    string cittaPartenza, string cittaArrivo, DateTime orarioPartenza, DateTime orarioArrivo)
     {
+        IdVolo = idVolo;
+        Aereo = aereo;
+        PostiRimanenti = postiRimanenti;
+        CostoDelPosto = costroDelPorsto;
+        CittaPartenza = cittaPartenza;
+        CittaArrivo = cittaArrivo;
+        OrarioPartenza = orarioPartenza;
+        OrarioArrivo = orarioArrivo;
     }
 }
